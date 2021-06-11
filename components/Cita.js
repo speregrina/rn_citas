5fac9bb66806bbc250cf31c49b1affe7c35a9ad5
Sergio Peregrina <habicito@gmail.com>
2021-06-11 12:41:52 -0500

    New change over the "Cita" component

--- components/Cita.js
+++ components/Cita.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 
 const Cita = ({item}) => {
     return (
-        <View>
-            <Text>{item.paciente}</Text>
+        <View style={styles.cita}>
+            <View>
+                <Text styles={styles.label}>ID: </Text>
+                <Text>{item.id}</Text>  
+            </View>
+            <View>
+                <Text styles={styles.label}>Paciente: </Text>
+                <Text>{item.paciente}</Text>  
+            </View>
+            <View>
+                <Text styles={styles.label}>Propietario: </Text>
+                <Text>{item.propietario}</Text>  
+            </View>
+            <View>
+                <Text>Sintomas: </Text>
+                <Text>{item.sintomas}</Text>  
+            </View>
         </View>
         )
 }
+
+const styles = StyleSheet.create({
+    cita:{
+        backgroundColor: '#FFF',
+        borderBottomColor: '#e1e1e1',
+        borderStyle: 'solid',
+        borderBottomWidth: 1,
+        paddingVertical: 20,
+        paddingHorizontal: 10
+    },
+    label:{
+
+    },
+    texto: {
+
+    }
+})
 
 export default Cita;
 
